cc4f4f2ad966ed6a1e68108b3abd785c972854fc
unknown <FarisET>
2024-05-20 17:07:09 +0500

    sql2 installed

--- src/databases/database.js
+++ src/databases/database.js
@@ -1,4 +1,5 @@
-var mysql= require('mysql');
+//var mysql= require('mysql');
+var mysql= require('mysql2');
 var fs = require('fs');
 
 // var con =mysql.createConnection({
@@ -23,13 +24,28 @@ var fs = require('fs');
 
 
 //local database
-var con =mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'-VGy$2jx',
-    database:'test1',
-})
+// var con =mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'-VGy$2jx',
+//     database:'test1',
+// })
 
+//AVN database
+var con = mysql.createConnection({
+    host: 'mysql-18e3438a-smdkhatri1-8514.b.aivencloud.com',
+    //port: 16298,
+    user: 'avnadmin',
+    password: 'AVNS_8t46WmbqkiS0u79D1GI',
+    port: 16294,
+    database: 'test1',
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync("./ca.pem").toString(),
+        //ca: process.env.MYSQL_AVN_SSL_CERT
+        //ca:fs.readFileSync('C:/Users/hp/Downloads/ca (1).pem')
+    }
+});
 
 con.connect(function(error){
     if(error){
